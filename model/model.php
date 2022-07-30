d38ca95183e5e4596f87149aa2006697046638d7
mathieu-tatat <91736481+mathieu-tatat@users.noreply.github.com>
2022-07-30 14:29:53 +0200

    Update model.php

--- model/model.php
+++ model/model.php
@@ -9,7 +9,7 @@
 /////////connection a la base de données////////
     try {
         $this->db = new PDO(
-            'mysql:host=localhost:3306;dbname=mathieu-tatat_paco ;charset=UTF-8', 'pacoPizz', 'pacoPizza',
+            'mysql:host=localhost:3306;dbname=mathieu-tatat_paco', 'pacoPizz', 'pacoPizza',
             [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
